42c29ac234184092bd1642c8f944db5ca83e5747
Mohammad Burhan <mohammadburhan@Mohammads-Mac-mini.local>
2022-02-01 09:50:42 +0800

    remove favourite feature

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 import movies from "./movies";
 import movie from "./movie";
-import favourites from "./favourites";
 import trivia from "./trivia";
 
 export default combineReducers({
   movies,
   movie,
-  favourites,
   trivia,
 });
